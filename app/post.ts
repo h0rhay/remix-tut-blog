3934da135ca03b3fb35a9347f2211ee1549f497f
George clark <george_clark@georges-mbp-2.home>
2022-02-15 17:50:23 +0000

    moar tut!

--- app/post.ts
+++ app/post.ts
@@ -1,15 +1,27 @@
 import path from 'path';
 import fs from 'fs/promises';
 import parseFrontMatter from 'front-matter';
+import invariant from "tiny-invariant";
+import { marked } from "marked";
 
 export type Post = {
     slug: string;
     title: string;
 };
 
+export type PostMarkdownAttributes = {
+    title: string;
+  };
+
 const postsPath = path.join(__dirname, "..","posts");
 
-export async function getPosts() {
+function isValidPostAttributes(
+    attributes: any
+  ): attributes is PostMarkdownAttributes {
+    return attributes?.title;
+  }
+
+  export async function getPosts() {
     const dir = await fs.readdir(postsPath);
     return Promise.all(
       dir.map(async filename => {
@@ -19,10 +31,26 @@ export async function getPosts() {
         const { attributes } = parseFrontMatter(
           file.toString()
         );
+        invariant(
+          isValidPostAttributes(attributes),
+          `${filename} has bad meta data!`
+        );
         return {
           slug: filename.replace(/\.md$/, ""),
           title: attributes.title
         };
       })
-    );  
-}
+    );
+  }
+
+  export async function getPost(slug: string) {
+    const filepath = path.join(postsPath, slug + ".md");
+    const file = await fs.readFile(filepath);
+    const { attributes, body } = parseFrontMatter(file.toString());
+    invariant(
+      isValidPostAttributes(attributes),
+      `Post ${filepath} is missing attributes`
+    );
+    const html = marked(body);
+  return { slug, html, title: attributes.title };
+  }
